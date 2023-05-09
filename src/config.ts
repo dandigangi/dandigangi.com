f197a7134ce431bddac2206859bfd6996567433e
Dan DiGangi <me@dandigangi.com>
2023-05-09 13:56:53 -0500

    No idea

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Captain Awesome";
+export const SITE_TITLE = "Dan DiGangi | Engineering Manager, Tech Mentor";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog! I write about modern web development.";
-export const TWITTER_HANDLE = "@yourtwitterhandle";
-export const MY_NAME = "Captain Awesome";
+  "Dan DiGangi is a Chicago based software engineering manager, tech mentor, and the organizer of the React Loop conference.";
+export const TWITTER_HANDLE = "@dandigangi";
+export const MY_NAME = "Dan DiGangi";
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
