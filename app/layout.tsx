9bfd877aec8232af37e485bb579d3dd4bf32f41d
Dan DiGangi <me@dandigangi.com>
2023-10-06 14:31:02 -0500

    Adding umami

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,25 +64,36 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       className={`${space_grotesk.variable} scroll-smooth`}
       suppressHydrationWarning
     >
-      <link rel="apple-touch-icon" sizes="76x76" href="/static/images/dan-digangi-logo-light.png" />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/static/images/dan-digangi-logo-light.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/static/images/dan-digangi-logo-light.png"
-      />
-      <link rel="manifest" href="/static/favicons/site.webmanifest" />
-      <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
-      <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
-      <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
-      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/static/images/dan-digangi-logo-light.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/static/images/dan-digangi-logo-light.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/static/images/dan-digangi-logo-light.png"
+        />
+        <link rel="manifest" href="/static/favicons/site.webmanifest" />
+        <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
+        <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
+        <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+        <script
+          async
+          src="https://analytics.eu.umami.is/script.js"
+          data-website-id="baae615d-8492-4d6f-abc6-b7dd992bdb2d"
+        ></script>
+      </head>
       <body className="bg-white text-black antialiased dark:bg-gray-950 dark:text-white">
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
