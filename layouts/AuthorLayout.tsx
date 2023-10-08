24e8af95bf57002940bdcf77bd948ad31cd356cb
Dan DiGangi <me@dandigangi.com>
2023-10-08 13:36:56 -0500

    Welcome to the game Pikachu

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -1,8 +1,11 @@
-import { ReactNode } from 'react'
+'use client'
+
+import { ReactNode, useState } from 'react'
 import type { Authors } from 'contentlayer/generated'
 import SocialIcon from '@/components/social-icons'
 import Image from '@/components/Image'
 import { github, linkedin, twitter } from '@/data/siteMetadata'
+import AvatarPikachu from '/public/static/images/avatar-pikachu.png'
 
 interface Props {
   children: ReactNode
@@ -11,6 +14,7 @@ interface Props {
 
 export default function AuthorLayout({ children, content }: Props) {
   const { name, avatar, occupation, company, email } = content
+  const [avatarDisplay, toggleAvatar] = useState(false)
 
   return (
     <>
@@ -22,9 +26,17 @@ export default function AuthorLayout({ children, content }: Props) {
         </div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
           <div className="flex flex-col items-center space-x-2 pt-8">
-            {avatar && (
+            {avatar && !avatarDisplay ? (
               <Image
                 src={avatar}
+                alt="avatar"
+                width={192}
+                height={192}
+                className="h-48 w-48 rounded-full"
+              />
+            ) : (
+              <Image
+                src={AvatarPikachu}
                 alt="avatar"
                 width={192}
                 height={192}
@@ -34,12 +46,18 @@ export default function AuthorLayout({ children, content }: Props) {
             <h3 className="pb-2 pt-4 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
             <div className="text-gray-500 dark:text-gray-400">{company}</div>
-            <div className="flex space-x-3 pt-6">
+            <div className="mb-5 flex space-x-3 pt-6">
               <SocialIcon kind="mail" href={`mailto:${email}`} />
               <SocialIcon kind="github" href={github} />
               <SocialIcon kind="linkedin" href={linkedin} />
               <SocialIcon kind="twitter" href={twitter} />
             </div>
+            <button
+              onClick={() => toggleAvatar(!avatarDisplay)}
+              className="text-sm uppercase underline font-semibold color text-violet-600"
+            >
+              {!avatarDisplay ? 'My Alter Ego' : 'Back to Dan'}
+            </button>
           </div>
           <div className="prose max-w-none pb-8 pt-8 dark:prose-invert xl:col-span-2">
             {children}
