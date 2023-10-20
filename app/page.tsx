b0917826caa707df009757ae2e69b09bdbf53c2e
Dan DiGangi <me@dandigangi.com>
2023-10-20 15:04:05 -0700

    Lead dev post, remove alert

--- app/page.tsx
+++ app/page.tsx
@@ -51,31 +51,6 @@ export default async function Page() {
           </p>
         </div>
         <br />
-        <div
-          className="flex items-center p-4 mb-4 text-yellow-800 border border-yellow-300 rounded-lg bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300 dark:border-yellow-800"
-          role="alert"
-        >
-          <svg
-            className="flex-shrink-0 inline w-4 h-4 mr-3"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-          >
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
-          </svg>
-          <div>
-            Attending{' '}
-            <a
-              target="_blank"
-              href="https://leaddev.com/leaddev-west-coast"
-              className="font-semibold underline hover:no-underline"
-            >
-              LeadDev West Coast
-            </a>{' '}
-            engineering manager conference Oct. 15-18th in SF/Oakland. Let's meet up!
-          </div>
-        </div>
         <div className="mt-8">
           <p className="mb-2 pt-3">
             Looking for new engineering management opportunities (Laid Off)
