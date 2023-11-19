1947383bbe27095403e2617855af62ccf39ff20a
Dan DiGangi <me@dandigangi.com>
2023-11-19 15:50:48 -0600

    Missed blog title

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,6 +2,7 @@ import ListLayout from '@/layouts/ListLayoutWithTags'
 import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 import { genPageMetadata } from 'app/seo'
+import PageHeader from '@/components/PageHeader'
 
 const POSTS_PER_PAGE = 5
 
@@ -22,12 +23,7 @@ export default function BlogPage() {
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Blog
-          </h1>
-        </div>
-
+        <PageHeader title="Blog" />
         <ListLayout
           posts={posts}
           initialDisplayPosts={initialDisplayPosts}
