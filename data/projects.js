b435cca3accbe6a1b51aa0044c526ed7b8352b5d
Dan DiGangi <me@dandigangi.com>
2024-01-31 08:34:12 -0600

    reorder projects

--- data/projects.js
+++ data/projects.js
@@ -1,5 +1,14 @@
 const projectsData = [
   {
+    title: 'Engineering Management Learning Resources',
+    description:
+      'A large collection of resources for engineering leaders and managers to grow their professional and technical skills.',
+    imgSrc: '/static/images/projects/engineering-management-learning-resources.png',
+    altText: 'Engineering Manager Learning Resources Project',
+    href: '/blog/engineering-manager-learning-resources',
+    linkText: 'View resources',
+  },
+  {
     title: 'Not Just UX',
     description:
       'Bite size collection of the good and bad UX found in our favorite products. Build a customer first mindset and create better experiences.',
@@ -9,13 +18,13 @@ const projectsData = [
     linkText: 'Coming soon',
   },
   {
-    title: 'Engineering Management Learning Resources',
+    title: 'React Loop',
     description:
-      'A large collection of resources for engineering leaders and managers to grow their professional and technical skills.',
-    imgSrc: '/static/images/projects/engineering-management-learning-resources.png',
-    altText: 'Engineering Manager Learning Resources Project',
-    href: '/blog/engineering-manager-learning-resources',
-    linkText: 'View resources',
+      "Organized Chicago's first and only ReactJS conference. Single day track bringing together a diverse group of speakers, developers, and enthusiasts.",
+    imgSrc: '/static/images/projects/react-loop-chicago-conference.png',
+    altText: "React Loop Project - Chicago's first and only ReactJS conference",
+    href: 'https://2019.reactloop.com',
+    linkText: null,
   },
   {
     title: 'Misc. Code Things',
@@ -26,15 +35,6 @@ const projectsData = [
       'Code snippets, minor projects, interview problems, lessons that supports my work to support the dev community.',
     href: 'https://github.com/dandigangi/misc-code-things',
     linkText: 'See the code',
-  },
-  {
-    title: 'React Loop',
-    description:
-      "Organized Chicago's first and only ReactJS conference. Single day track bringing together a diverse group of speakers, developers, and enthusiasts.",
-    imgSrc: '/static/images/projects/react-loop-chicago-conference.png',
-    altText: "React Loop Project - Chicago's first and only ReactJS conference",
-    href: 'https://2019.reactloop.com',
-    linkText: null,
   },
 ]
 
