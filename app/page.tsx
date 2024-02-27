b7d3af86895657287337f765ad8beb7ead41f5d5
Dan DiGangi <me@dandigangi.com>
2024-02-27 14:04:20 -0600

    previous logos

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
 import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 import Main from './Main'
+import Link from 'next/link'
+import Image from 'next/image'
+
+import DocuSignLogo from '/public/static/images/xp/docusign.png'
+import ApartmentsComLogo from '/public/static/images/xp/apartmentscom.png'
+import ArriveLogisticsLogo from '/public/static/images/xp/arrivelogistics.png'
+import ActiveCampaignLogo from '/public/static/images/xp/activecampaign.png'
+import OpenLaneLogo from '/public/static/images/xp/openlane.png'
+import { height } from 'tailwindcss/defaultTheme'
 
 // Page: Home
 export default async function Page() {
@@ -21,8 +30,7 @@ export default async function Page() {
             <em>Build experiences, not software</em>
           </p>
         </div>
-        <br />
-        <div className="mt-8">
+        <div className="mt-8 pt-4">
           <p className="mb-2 pt-3">
             Looking for new engineering management opportunities (Laid Off)
           </p>
@@ -49,6 +57,75 @@ export default async function Page() {
               LinkedIn &rarr;
             </a>
           </p>
+        </div>
+        <div className="pb-4">
+          <h2 className="pt-8 mb-4 text-sm text-gray-900 dark:text-white sm:text-sm sm:text-sm">
+            [ PREVIOUSLY ]
+          </h2>
+          <div className="grid grid-cols-5 gap-6">
+            <div>
+              <Link href="https://docusign.com" aria-label="Link to DocuSign" target="_blank">
+                <Image
+                  alt={'Previously worked at DocuSign'}
+                  src={DocuSignLogo}
+                  width={544}
+                  height={306}
+                />
+              </Link>
+            </div>
+            <div className="relative right-12">
+              <Link
+                href="https://apartments.com"
+                aria-label="Link to Apartments.com"
+                target="_blank"
+              >
+                <Image
+                  alt={'Previously worked at Apartments.com'}
+                  src={ApartmentsComLogo}
+                  width={544}
+                  height={306}
+                />
+              </Link>
+            </div>
+            <div className="relative right-8">
+              <Link
+                href="https://activecampaign.com"
+                aria-label="Link to Active Campaign"
+                target="_blank"
+              >
+                <Image
+                  alt={'Previously worked at Active Campaign'}
+                  src={ActiveCampaignLogo}
+                  width={544}
+                  height={306}
+                />
+              </Link>
+            </div>
+            <div className="relative right-2">
+              <Link
+                href="https://arrivelogistics.com"
+                aria-label="Link to DocuSign"
+                target="_blank"
+              >
+                <Image
+                  alt={'Previously worked at Arrive Logistics'}
+                  src={ArriveLogisticsLogo}
+                  width={544}
+                  height={306}
+                />
+              </Link>
+            </div>
+            <div>
+              <Link href="https://openlane.com" aria-label="Link to OpenLane" target="_blank">
+                <Image
+                  alt={'Previously worked at OpenLane'}
+                  src={OpenLaneLogo}
+                  width={544}
+                  height={306}
+                />
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
       <Main posts={posts} />
